8069c76f386ec8978ff30a1805898548a338dbd2
jaredstander <jaredstander@gmail.com>
2013-07-30 19:17:45 -0700

    Added .gitignore file for client keys from Github.

--- config/environment.rb
+++ config/environment.rb
@@ -31,3 +31,9 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+env_config = YAML.load_file(APP_ROOT.join('config', 'github.yaml'))
+
+env_config.each do |key, value|
+  ENV[key] = value
+end
